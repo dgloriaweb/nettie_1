bd14f58fc63122f0ee98659cd3adefc7331e37b8
unknown <dgloria.web@gmail.com>
2020-03-09 17:46:28 +0000

    added home page and team

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,18 @@
 
 <h2>Parenting Advice & Workshops</h2>
 <br>
+<!--  **********************  -->
 <h2>Life Coaching</h2>
 <br>
+<!--  **********************  -->
 <h2>Testimonials</h2>
 <br>
+<!--  **********************  -->
 <h2>The Team</h2>
 <br>
+
+@include('inc.team')
+
+<!--  **********************  -->
 <h2>Service Pricing</h2>
 <br>
---
+++ resources/views/inc/team.php
@@ -0,0 +1,92 @@
+<h3>ABOUT The Company</h3>
+<p>Challenging-Behaviour Counselling & Wellbeing Services was Founded in 2015. Challenging-Behaviour  Counselling Wellbeing Foundation was Founded in 2017.</p>
+
+<p>At Challenging-Behaviour we are focused on providing Person Centered Experience.</p>
+
+<p>We work within a Person Centered Approach using variety of styles of Creative therapy, Behaviour Strategies, General Psychology, Psychoeducation, Counselling and Life coach therapies. </p>
+
+<p>Feel free to look around our website and if you have any comments or questions, please do contact us. Check back later for new updates to our website. There's much more to come</p>
+
+<h3>"Friends of Challenging-Behaviour"</h3>
+
+<p>This is our Fundraising department for our charitable company Challenging-Behaviour Counselling Wellbeing Foundation. We are able subsidise people with discounted or entirely.</p> 
+<p>
+    Please send requests to: 
+    <a href="mailto:Contact@FriendsofChallenging-behaviour.org.uk">
+    Contact@FriendsofChallenging-behaviour.org.uk
+    </a>
+</p>    
+
+<p>If you would like to donate you can do through Paypal challenging-behaviour@hotmail.co.uk</p>
+
+<p>
+    (There is a criteria to be met for discounted and free Counselling, sadly funds are limited so all the people asking will not get full funding)
+</p>
+
+<h3>ABOUT The Founder: Nettie Forsyth</h3> 
+<h4>NNEB, Dip Child Dev Psych, BSc Psych (Hon), OA Dip Crim.</h4> 
+<p>Nettie started her Career with Children and families in 1985. She has since gained a vast amount of knowledge within Attachment, Families, Challenging Behaviour, Play, Adoption, ASD Spectrum and Extreme life behaviours. Working in a variety of services Nursery, School, Private home, Film industry, Hospital and Community, Women Centre, Youth and Babies.</p>
+
+<p>Qualifying in varying subjects and courses within the topic of Children, families, Behaviour.</p>
+
+<h4>Qualifications:-</h4>
+
+<li>2015 Level 4 Diploma in Counselling and Psychotherapy. 2015 BACP Certificate of Proficiency</li>
+<li>2015 Foundation Degree in Counselling.  </li>
+<li>2015 NVQ2 in ASD</li>
+<li>2014 NCFE Criminology for Beginners level 2, 2014 OA Diploma Criminology for Beginners Distinction, </li>
+<li>2014 Reiki Level 1, 2015 Reiki Level 2</li>
+<li>2014 Hands on Baby Massage instructor, 2014 Hands on Baby Yoga instructor. </li>
+<li>2013 Exploring psychology, 2013 Exploring psychology research project</li>
+<li>2013 BSc (Hon) Psychology, </li>
+<li>2012 NCFE level 2 Counselling skills, 2013 Cpcab Level 3 in Counselling skills, </li>
+<li>2012 Social Psychology, 2012 Cognitive Psychology, 2012 Biological Psychology</li>
+<li>2011 Understanding the Autism Spectrum. 2015 Certificate in Autism</li>
+<li>2011 Solihull Approach First five Years, 2011 Solihull Approach School Years</li>
+<li>2011 Social Science. 2011 Child Development </li>
+<li>2011 Introduction to counselling</li>
+<li>2010 Counselling Fear and Sadness, </li>
+<li>2005 Protective Behaviour</li>
+<li>2002 Diploma in Child Development Psychology</li>
+<li>1987 NNEB, 1995 Learning Through play, </li>
+
+
+
+
+<h4>Continue Professional Development (CPD) courses.</h4> 
+<li>2016 Safeguarding Children 3.</li>
+<li>2016 Safeguarding Adults 3 </li>
+<li>2015 Working with Youths</li>
+<li>2015 Sexual abuse. 2015 KC Safeguarding. </li>
+<li>2014 Solihull Brain Development, </li>
+<li>2014 Foundation in NLP, </li>
+<li>2013 Relationship support</li>
+<li>2013 Enabling children and young people to develop and sustain self-esteem</li>
+<li>2013 Alcohol Training, </li>
+<li>2010 Mill pond Sleep Training, </li>
+<li>2009 Locked in Rage and Hate, 2009 Anxious worried Children, 2010 One Life -Margot Sunderland, </li>
+<li>2009 Attachment and Resilience, </li>
+<li>2008 Parenting the Adopted child, 2008 Telephone help skills, 2009 MARAC training, </li>
+<li>2008 Managing Challenging Behaviour ASD</li>
+<li>2002 Protective behaviour </li>
+<li>2002 Challenging behaviour, </li>
+
+
+<h4>NHS Training and CPD</h4>
+
+<p>Infection control, Initiating Breast Feeding, Safeguarding vulnerable Adults, Child protection BSCB</p>
+<p>Protecting disabled children BSCB, Working with Challenging Families BSCB, Working together in safeguarding children BSCB, Sex Matters Brook training, Care  planning advanced, Moving and Handling, Equality and Diversity, Physical abuse, Assault avoidance</p>
+<p>Conflict resolution, Understanding child sexual abuse BSCB, Effective Core Groups BSCB</p>
+<p>Emotional abuse and child neglect BSCB, Domestic violence and child protection BSCB</p>
+<p>Impact of Parental Substance abuse BSCB, Mentally ill parents and their children BSCB</p>
+<p>RIO 1 and RIO 2, Mental Capacity Act, Family Star Training, Anger, Counselling Children and Adolescence, Creative therapy.</p>
+
+<h4>Other Essential Credentials</h4>
+
+<p>First Aid 2016, DBS -Current</p>
+
+<h3>Memberships:</h3>
+
+<p>Registered as Graduate Member with the BPS (British Psychological Society)</p>
+
+<p>Membership of the BACP (British Association of Counselling and Psychotherapy)</p>
