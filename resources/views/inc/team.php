356e97700d832a22bbe045c260344a8abcdec7f8
unknown <dgloria.web@gmail.com>
2020-03-10 12:26:39 +0000

    rewrite team page

--- resources/views/inc/team.php
+++ resources/views/inc/team.php
@@ -1,75 +1,57 @@
-<h3>ABOUT The Company</h3>
-<p>Challenging-Behaviour Counselling & Wellbeing Services was Founded in 2015. Challenging-Behaviour  Counselling Wellbeing Foundation was Founded in 2017.</p>
+<h1>Meet the team</h1>
+<p>Members of our team; Each Therapist works as an individual. Each Therapist adheres to the membership rules of practice to their awarding body BACP. Each Therapist is  DBS checked. Each Therapist is insured. Each Therapist works within the safe, warm supportive environment of Challenging-behaviour. Each Therapist is accountable for confidentiality and an ethical code of practice and to adhere to the companies policies and procedures.</p>
 
-<p>At Challenging-Behaviour we are focused on providing Person Centered Experience.</p>
+<!--  NETTIE -->
+<h2>About the founder: Nettie Forsyth</h2> 
+<p>I am a Person centred Therapist and a Behaviour Strategist.
+Working over 30 years in the world of Families; Adults and Children has given me the knowledge and experience I have.</p>
+<p>My aspiration is to have a Behaviour Centre that works with all ages and stages of a Humans development. Challenging the aspects of a Humans behaviour both its internal and external enabling a person to become their organismic self.</p>
+<p>I feel very privileged to be working and have worked with so many people; empowering them to move on.</p>
 
-<p>We work within a Person Centered Approach using variety of styles of Creative therapy, Behaviour Strategies, General Psychology, Psychoeducation, Counselling and Life coach therapies. </p>
-
-<p>Feel free to look around our website and if you have any comments or questions, please do contact us. Check back later for new updates to our website. There's much more to come</p>
-
-<h3>"Friends of Challenging-Behaviour"</h3>
-
-<p>This is our Fundraising department for our charitable company Challenging-Behaviour Counselling Wellbeing Foundation. We are able subsidise people with discounted or entirely.</p> 
-<p>
-    Please send requests to: 
-    <a href="mailto:Contact@FriendsofChallenging-behaviour.org.uk">
-    Contact@FriendsofChallenging-behaviour.org.uk
-    </a>
-</p>    
-
-<p>If you would like to donate you can do through Paypal challenging-behaviour@hotmail.co.uk</p>
-
-<p>
-    (There is a criteria to be met for discounted and free Counselling, sadly funds are limited so all the people asking will not get full funding)
-</p>
-
-<h3>ABOUT The Founder: Nettie Forsyth</h3> 
-<h4>NNEB, Dip Child Dev Psych, BSc Psych (Hon), OA Dip Crim.</h4> 
+<p>NNEB, Dip Child Dev Psych, BSc Psych (Hon), OA Dip Crim.</p> 
 <p>Nettie started her Career with Children and families in 1985. She has since gained a vast amount of knowledge within Attachment, Families, Challenging Behaviour, Play, Adoption, ASD Spectrum and Extreme life behaviours. Working in a variety of services Nursery, School, Private home, Film industry, Hospital and Community, Women Centre, Youth and Babies.</p>
 
 <p>Qualifying in varying subjects and courses within the topic of Children, families, Behaviour.</p>
 
-<h4>Qualifications:-</h4>
+<h4>Qualifications:</h4>
 
 <li>2015 Level 4 Diploma in Counselling and Psychotherapy. 2015 BACP Certificate of Proficiency</li>
-<li>2015 Foundation Degree in Counselling.  </li>
+<li>2015 Foundation Degree in Counselling  </li>
 <li>2015 NVQ2 in ASD</li>
-<li>2014 NCFE Criminology for Beginners level 2, 2014 OA Diploma Criminology for Beginners Distinction, </li>
+<li>2014 NCFE Criminology for Beginners level 2, 2014 OA Diploma Criminology for Beginners Distinction </li>
 <li>2014 Reiki Level 1, 2015 Reiki Level 2</li>
-<li>2014 Hands on Baby Massage instructor, 2014 Hands on Baby Yoga instructor. </li>
+<li>2014 Hands on Baby Massage instructor, 2014 Hands on Baby Yoga instructor </li>
 <li>2013 Exploring psychology, 2013 Exploring psychology research project</li>
-<li>2013 BSc (Hon) Psychology, </li>
-<li>2012 NCFE level 2 Counselling skills, 2013 Cpcab Level 3 in Counselling skills, </li>
+<li>2013 BSc (Hon) Psychology </li>
+<li>2012 NCFE level 2 Counselling skills, 2013 Cpcab Level 3 in Counselling skills </li>
 <li>2012 Social Psychology, 2012 Cognitive Psychology, 2012 Biological Psychology</li>
 <li>2011 Understanding the Autism Spectrum. 2015 Certificate in Autism</li>
 <li>2011 Solihull Approach First five Years, 2011 Solihull Approach School Years</li>
 <li>2011 Social Science. 2011 Child Development </li>
 <li>2011 Introduction to counselling</li>
-<li>2010 Counselling Fear and Sadness, </li>
+<li>2010 Counselling Fear and Sadness </li>
 <li>2005 Protective Behaviour</li>
 <li>2002 Diploma in Child Development Psychology</li>
-<li>1987 NNEB, 1995 Learning Through play, </li>
+<li>1987 NNEB, 1995 Learning Through play </li>
 
 
-
-
-<h4>Continue Professional Development (CPD) courses.</h4> 
-<li>2016 Safeguarding Children 3.</li>
+<h4>Continue Professional Development (CPD) courses</h4> 
+<li>2016 Safeguarding Children 3</li>
 <li>2016 Safeguarding Adults 3 </li>
 <li>2015 Working with Youths</li>
-<li>2015 Sexual abuse. 2015 KC Safeguarding. </li>
-<li>2014 Solihull Brain Development, </li>
-<li>2014 Foundation in NLP, </li>
+<li>2015 Sexual abuse. 2015 KC Safeguarding </li>
+<li>2014 Solihull Brain Development </li>
+<li>2014 Foundation in NLP </li>
 <li>2013 Relationship support</li>
 <li>2013 Enabling children and young people to develop and sustain self-esteem</li>
-<li>2013 Alcohol Training, </li>
-<li>2010 Mill pond Sleep Training, </li>
-<li>2009 Locked in Rage and Hate, 2009 Anxious worried Children, 2010 One Life -Margot Sunderland, </li>
-<li>2009 Attachment and Resilience, </li>
-<li>2008 Parenting the Adopted child, 2008 Telephone help skills, 2009 MARAC training, </li>
+<li>2013 Alcohol Training </li>
+<li>2010 Mill pond Sleep Training </li>
+<li>2009 Locked in Rage and Hate, 2009 Anxious worried Children 2010 One Life -Margot Sunderland, </li>
+<li>2009 Attachment and Resilience </li>
+<li>2008 Parenting the Adopted child, 2008 Telephone help skills, 2009 MARAC training </li>
 <li>2008 Managing Challenging Behaviour ASD</li>
 <li>2002 Protective behaviour </li>
-<li>2002 Challenging behaviour, </li>
+<li>2002 Challenging behaviour </li>
 
 
 <h4>NHS Training and CPD</h4>
@@ -90,3 +72,14 @@
 <p>Registered as Graduate Member with the BPS (British Psychological Society)</p>
 
 <p>Membership of the BACP (British Association of Counselling and Psychotherapy)</p>
+
+<!-- NETTIE END -->
+
+<h2>Neo</h2>
+
+<h3>Emotional Support Animal</h3>. 
+
+<p>Neo joined us in January 2018 as a training ESA dog to work alongside Nettie.</p>
+<p>You can follow him https://www.facebook.com/The-Adventures-of-Neo-176290363206343</p>
+<p>(Clients can choose to have Neo in the room or not)</p>
+
