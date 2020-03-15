b0652b6aed9d2bc3406a81e389428984bbc2b586
unknown <dgloria.web@gmail.com>
2020-03-15 13:40:35 +0000

    processed requested modifications from clients notes

---
+++ resources/views/spare_stuff.php
@@ -0,0 +1,120 @@
+<!--  **********************  -->
+<div class="parenting" id="parenting">
+    <h2>Parenting Advice & Workshops</h2>
+<div class="container">
+    <h4>Parenting workshops</h4>
+    <p>Watch out for information. If there is specific parenting needs then let us know by contacting us.</p>
+
+    <p>Topics:</p>
+    <p>
+        <ul>
+            <li>ASD</li>
+            <li>Stealing</li>
+            <li>Tantrums</li>
+            <li>Play</li>
+            <li>Discipline versus Punishment</li>
+
+        </ul>
+    </p>
+</div>
+
+</div>
+<!--  **********************  -->
+<hr>
+<div class="life_coaching" id="life_coaching">
+    <h2>Life Coaching</h2>
+    <div class="container">
+
+    <p>Lost your way? Need to discuss and get practical solutions? Then contact us.</p>
+</div>
+</div>
+<!--  **********************  -->
+<hr>
+<div class="" id="">
+    <h2>Behaviour Strategies</h2>
+    <div class="container">
+
+    <h3>What is Behaviour Strategy work?</h3>
+    <p>Behaviour Strategy work is to help those who have behaviours that are not suiting themselves or suiting situations within society. It is a strategy to help you manage.</p>
+
+    <p>What behaviours do you find Strategies for?</p>
+    <ul>
+        <li>Anxiety strategies</li>
+        <li>Sleep strategies</li>
+        <li>Anger management and strategies</li>
+        <li>Historic negative behaviour patterns</li>
+        <li>Tantrum strategies</li>
+        <li>Weaning strategies</li>
+        <li>Anger strategies</li>
+        <li>Toilet training strategies</li>
+        <li>Relationship strategies</li>
+    </ul>
+</div>
+</div>
+
+
+
+
+
+<!-- Nettie  -->
+
+<h5>Qualifications:</h5>
+
+<li>2015 Level 4 Diploma in Counselling and Psychotherapy. 2015 BACP Certificate of Proficiency</li>
+<li>2015 Foundation Degree in Counselling  </li>
+<li>2015 NVQ2 in ASD</li>
+<li>2014 NCFE Criminology for Beginners level 2, 2014 OA Diploma Criminology for Beginners Distinction </li>
+<li>2014 Reiki Level 1, 2015 Reiki Level 2</li>
+<li>2014 Hands on Baby Massage instructor, 2014 Hands on Baby Yoga instructor </li>
+<li>2013 Exploring psychology, 2013 Exploring psychology research project</li>
+<li>2013 BSc (Hon) Psychology </li>
+<li>2012 NCFE level 2 Counselling skills, 2013 Cpcab Level 3 in Counselling skills </li>
+<li>2012 Social Psychology, 2012 Cognitive Psychology, 2012 Biological Psychology</li>
+<li>2011 Understanding the Autism Spectrum. 2015 Certificate in Autism</li>
+<li>2011 Solihull Approach First five Years, 2011 Solihull Approach School Years</li>
+<li>2011 Social Science. 2011 Child Development </li>
+<li>2011 Introduction to counselling</li>
+<li>2010 Counselling Fear and Sadness </li>
+<li>2005 Protective Behaviour</li>
+<li>2002 Diploma in Child Development Psychology</li>
+<li>1987 NNEB, 1995 Learning Through play </li>
+
+
+<h5>Continue Professional Development (CPD) courses</h5> 
+<li>2016 Safeguarding Children 3</li>
+<li>2016 Safeguarding Adults 3 </li>
+<li>2015 Working with Youths</li>
+<li>2015 Sexual abuse. 2015 KC Safeguarding </li>
+<li>2014 Solihull Brain Development </li>
+<li>2014 Foundation in NLP </li>
+<li>2013 Relationship support</li>
+<li>2013 Enabling children and young people to develop and sustain self-esteem</li>
+<li>2013 Alcohol Training </li>
+<li>2010 Mill pond Sleep Training </li>
+<li>2009 Locked in Rage and Hate, 2009 Anxious worried Children 2010 One Life -Margot Sunderland </li>
+<li>2009 Attachment and Resilience </li>
+<li>2008 Parenting the Adopted child, 2008 Telephone help skills, 2009 MARAC training </li>
+<li>2008 Managing Challenging Behaviour ASD</li>
+<li>2002 Protective behaviour </li>
+<li>2002 Challenging behaviour </li>
+
+
+<h5>NHS Training and CPD</h5>
+
+<p>Infection control, Initiating Breast Feeding, Safeguarding vulnerable Adults, Child protection BSCB</p>
+<p>Protecting disabled children BSCB, Working with Challenging Families BSCB, Working together in safeguarding children BSCB, Sex Matters Brook training, Care  planning advanced, Moving and Handling, Equality and Diversity, Physical abuse, Assault avoidance</p>
+<p>Conflict resolution, Understanding child sexual abuse BSCB, Effective Core Groups BSCB</p>
+<p>Emotional abuse and child neglect BSCB, Domestic violence and child protection BSCB</p>
+<p>Impact of Parental Substance abuse BSCB, Mentally ill parents and their children BSCB</p>
+<p>RIO 1 and RIO 2, Mental Capacity Act, Family Star Training, Anger, Counselling Children and Adolescence, Creative therapy.</p>
+
+<h5>Other Essential Credentials</h5>
+
+<p>First Aid 2016, DBS -Current</p>
+
+<h4>Memberships:</h4>
+
+<p>Registered as Graduate Member with the BPS (British Psychological Society)</p>
+
+<p>Membership of the BACP (British Association of Counselling and Psychotherapy)</p>
+
